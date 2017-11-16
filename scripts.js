0eada82b9db40fd0f9757ca34fe4377293daadc0
Krzysztof Kleina <k.kleina@wp.eu>
2017-11-16 10:11:23 +0100

    Add button

--- scripts.js
+++ scripts.js
@@ -32,4 +32,25 @@ $(function() {
   iPhone6Splus.printInfo();
   OnePlusOne.printInfo();
 
+// Add button - m11.5
+
+  function Button(text) {
+    this.text = text || 'Hello';
+  }
+
+  Button.prototype = {
+    create: function() {
+      var self = this;
+      this.$element = $('<button>');
+      this.$element.text(this.text);
+      this.$element.click(function() {
+        alert(self.text);
+      });
+      this.$element.appendTo($('body'));
+    }
+  }
+
+  var btn1 = new Button('Big button :)');
+  btn1.create();
+
 });
